32707d518bc3c4a7e25f8b3c888dd77e76a82ab4
Drelf2018 <drelf2018@outlook.com>
2022-07-29 07:51:32 +0800

    feat: 新增配置文件

---
+++ database.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+var jsonpath = "./content.json"
+
+func LoadContent() {
+	iot, err := ioutil.ReadFile(jsonpath) //读取文件
+	if checkErr(err) {                    //做错误判断
+		json.Unmarshal(iot, &MbText)
+	}
+}
+
+func SaveContent() {
+	if js, err := json.MarshalIndent(MbText, "", "	"); checkErr(err) {
+		err := ioutil.WriteFile(jsonpath, js, 0664) //接收文件名，字节类型的数据，文件的权限
+		checkErr(err)
+	}
+}
+
+func LoadConfig() {
+	//读取yaml文件到缓存中
+	config, err := ioutil.ReadFile("spider.yaml")
+	checkErr(err)
+	//yaml文件内容影射到结构体中
+	err = yaml.Unmarshal(config, &cfg)
+	checkErr(err)
+}
+
+type Config struct {
+	Account struct {
+		Cookie string `yaml:"cookie"`
+		UID    int    `yaml:"uid"`
+	} `yaml:"account"`
+	Message struct {
+		Comment string `yaml:"comment"`
+		Online  string `yaml:"online"`
+		Weibo   string `yaml:"weibo"`
+	} `yaml:"message"`
+	Ws struct {
+		Host string `yaml:"host"`
+		Post int    `yaml:"post"`
+	} `yaml:"ws"`
+}
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
@@ -21,26 +21,49 @@ func checkErr(err error) bool {
 	return true
 }
 
-// 微博请求头与 cookie 信息
+type LogFormatter struct{}
+
+// code from: https://zhuanlan.zhihu.com/p/538240645
+func (m *LogFormatter) Format(entry *log.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+
+	timestamp := entry.Time.Format("15:04:05")
+	msg := fmt.Sprintf("[%s] [%s] %s\n", timestamp, entry.Level, entry.Message)
+
+	b.WriteString(msg)
+	return b.Bytes(), nil
+}
+
+// 微博请求头
 var req *http.Request
-var cookie = "SCF=Ao_CfNDAVoOOnPeRbWYncYwIxkt1qdMnP8GJwYAFUbdV2AwNmv18NfGuAVLGkp_al91dOQIJTvviNKRX_WnQV0I.; SUB=_2A25P0FJVDeRhGeFM7lQU8i7PyjmIHXVtO34drDV6PUJbktCOLRHBkW1NQNxRTyundxN1rIEaDb8vuDu9LX1y2PfV; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWsM7qnLH5XXeUsRC8WX5b75NHD95QNeo-cSKz7e02fWs4DqcjPi--RiKnXiKnci--4i-zEi-2ReKzpe0nt; _T_WM=b7e06bb363bcddabb34f5e7aba1d13f8; WEIBOCN_FROM=1110006030; MLOGIN=1; XSRF-TOKEN=56db96; mweibo_short_token=b93d1a8e49; M_WEIBOCN_PARAMS=from=page_100808&luicode=10000011&lfid=1008081a127e1db26d4483eadf1d1dbe1a80c2_-_live&fid=1008081a127e1db26d4483eadf1d1dbe1a80c2_-_live&uicode=10000011"
 
 // 用于对比微博是否编辑的集合 key: bid val: content
 var MbText = make(map[string]string)
 
+// 配置文件
+var cfg Config
+
 // 避免同时调用 python 库的等待组
 var wg sync.WaitGroup
 
-// 为了解决等待组中 goroutine 超过 2 引发恐慌(panic) 而引入的计数器
+// 为了解决等待组中 goroutine 超过 2 引发恐慌(panic)而引入的计数器
 // 虽然它的存在让代码更臃肿了 但是本人 golang 苦手 只能这样操作
 var Tasks int
 
 // 初始化请求会话并在请求头中加入 cookie
 func init() {
-	url := "https://m.weibo.cn/api/container/getIndex?from=page_100808&mod[]=TAB%3Ffrom%3Dpage_100808&mod[]=TAB&containerid=1008081a127e1db26d4483eadf1d1dbe1a80c2_-_live"
+	LoadContent()
+	LoadConfig()
+	log.SetFormatter(&LogFormatter{})
+	url := fmt.Sprintf("https://m.weibo.cn/api/container/getIndex?containerid=107603%v", cfg.Account.UID)
 	var err error
 	if req, err = http.NewRequest("GET", url, nil); err == nil {
-		req.Header.Add("cookie", cookie)
+		req.Header.Add("cookie", cfg.Account.Cookie)
 		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
 	} else {
 		panic(err)
@@ -62,38 +85,69 @@ func GetIndex() (res gothon.Result) {
 	return
 }
 
-// 这个人的微博也查(query) 一下吧
+// 这个人的微博也查(query)一下吧
 func Query() {
+	log.Info("开始查询")
 	index := GetIndex()
 	for _, card := range index.Data.Cards {
-		if c := card.CardGroup; c != nil {
-			switch c[1].CardType {
-			// 发微博或评论
-			case "9":
-				if strings.Contains(c[0].Desc, "评论") {
-					// 找评论去了
-				} else {
-					// 如果 未记录过该微博 或者 内容有编辑 的同时满足 生成图片任务数小于 2 时
-					if SavedText, ok := MbText[c[1].Mblog.Bid]; (!ok || SavedText != c[1].Mblog.Text) && Tasks < 2 {
-						fmt.Println(c[1].Mblog.Bid, "start")
-						ch := make(chan string)
-						Tasks += 1
+		switch card.CardType {
+		// 发微博或评论
+		case 9:
+			// 如果 未记录过该微博 或者 内容有编辑 的同时满足 生成图片任务数小于 2 时
+			if SavedText, ok := MbText[card.Mblog.Bid]; (!ok || SavedText != card.Mblog.Text) && Tasks < 2 {
+				log.Info("更新微博", card.Mblog.Bid, card.Mblog.Text)
+				ch := make(chan string)
+				Tasks += 1
 
-						// 生成图片 生成完毕通过 ch 返回该任务微博bid
-						go gothon.CreateNewImg(&wg, ch, c[1].Mblog, cookie)
+				// 生成图片 生成完毕通过 ch 返回该任务微博bid
+				go gothon.CreateNewImg(&wg, ch, card.Mblog, cfg.Account.Cookie)
 
-						// 更新此微博的内容
-						go func(ch chan string, NewText string) {
-							MbText[<-ch] = NewText
-							Tasks -= 1
-							// send_msg
-						}(ch, c[1].Mblog.Text)
-					}
-				}
-			case "30": // 在线状态
-				fmt.Println(c[1].Desc1)
+				// 更新此微博的内容
+				go func(ch chan string, NewText string) {
+					bid := <-ch
+					MbText[bid] = NewText
+					go SaveContent()
+					log.Info("完成更新", bid)
+					Tasks -= 1
+					// send_msg
+				}(ch, card.Mblog.Text)
 			}
 		}
+	}
+}
+
+type OnlineStatus struct {
+	Data struct {
+		Cards []struct {
+			CardGroup []struct {
+				Desc1 *string `json:"desc1,omitempty"`
+			} `json:"card_group,omitempty"`
+		} `json:"cards,omitempty"`
+	} `json:"data"`
+}
+
+func Online() func() string {
+	var last_online_status string
+	url := "https://m.weibo.cn/api/container/getIndex?from=page_100808&mod[]=TAB%3Ffrom%3Dpage_100808&mod[]=TAB&containerid=1008081a127e1db26d4483eadf1d1dbe1a80c2_-_live"
+	return func() string {
+		if resp, err := http.Get(url); checkErr(err) {
+			defer resp.Body.Close()
+			var status OnlineStatus
+			if body, err := ioutil.ReadAll(resp.Body); checkErr(err) {
+				if err := json.Unmarshal(body, &status); checkErr(err) {
+					for _, card := range status.Data.Cards {
+						if len(card.CardGroup) > 1 && card.CardGroup[1].Desc1 != nil {
+							if last_online_status != *card.CardGroup[1].Desc1 {
+								last_online_status = *card.CardGroup[1].Desc1
+								log.Info(last_online_status)
+								return last_online_status
+							}
+						}
+					}
+				}
+			}
+		}
+		return ""
 	}
 }
 
@@ -102,10 +156,12 @@ func main() {
 	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
+	OnlineStatus := Online()
 	// 提前执行一次任务 急 等不了了
 	Query()
 	for range ticker.C {
-		Query()
+		go Query()
+		go OnlineStatus()
 	}
 
 }
